22447da116b3508ad7aed4f3ad540ef9463981b1
Aun Khan <107195320+ownkhan125@users.noreply.github.com>
2024-10-21 11:50:49 +0500

    Add files via upload

---
+++ math Formula/binanceProfitCalculate.js
@@ -0,0 +1,16 @@
+
+initial_coin_price = 60000 
+investment_amount = 10  
+new_coin_price = 62000  
+
+// Calculate how much of the coin was purchased initially
+coin_amount_purchased = investment_amount / initial_coin_price
+
+// Calculate the value of the purchased coin amount at the new price
+new_investment_value = coin_amount_purchased * new_coin_price
+
+// Calculate the profit
+profit = new_investment_value - investment_amount
+profit
+Result
+0.33333333333333215
